fed2aca352f11e90ffe897e1d61814b9e16d7a6c
isagarazadeh <garazadeisa625@gmail.com>
2024-01-26 13:42:53 -0800

    10th

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def add(a, b):
+    if b < 0:
+        b = -1 * (-1 * b)
+    elif a < 0:
+        a = -1 * (-1 * a)
     c = a + b
     print("{}".format(c), end="")
     return c
