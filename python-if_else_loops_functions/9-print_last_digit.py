42cc0ff6759b21074667735dd1aa60960f046004
isagarazadeh <garazadeisa625@gmail.com>
2024-01-26 13:35:10 -0800

    9th

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     if number < 0:
         number = -1 * number
     last = number % 10
-    print("{}".format(last))
+    print("{}".format(last), end="")
     return last
