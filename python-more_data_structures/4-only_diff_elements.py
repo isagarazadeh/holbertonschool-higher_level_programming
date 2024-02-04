39da532e1d8442206e8525c62651ae57090e9a13
isagarazadeh <garazadeisa625@gmail.com>
2024-02-04 08:14:40 -0800

    4th

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    '''od_set = []
-    for i in set_1:
-        for j in set_2:
-            if j != i:
-                od_set.append(i)'''
-    if od_set != set_1.intersection(set_2):
-        return od_set
+    od_set = set_1.smymetric_difference(set_2)
+    return od_set
