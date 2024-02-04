0729e7f77f6a8d3fc1acc7a735ce4a3f49f29dba
isagarazadeh <garazadeisa625@gmail.com>
2024-02-04 06:38:39 -0800

    2nd task

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     result = []
+    summa = 0
     result = my_list.reduce()
-    print("Result: ".format(result))
+    for i in result:
+        summa += i
+    print("Result: ".format(summa))
