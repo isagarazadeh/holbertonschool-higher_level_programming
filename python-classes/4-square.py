07a35c1a4172a174890069913c87f9a1d2ed0660
isagarazadeh <garazadeisa625@gmail.com>
2024-02-04 11:17:06 -0800

    5th

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''Square with private size attribute and check'''
+
+
+class Square:
+    '''Gets size as its attribute'''
+    def __init__(self, size=0):
+        '''
+        Init block of Square Class
+        '''
+        self.size = size
+
+    def area(self):
+        '''Calculates the area of square'''
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        '''size getter'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''
+        size setter
+
+        Args:
+            value: value of the size
+        '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
