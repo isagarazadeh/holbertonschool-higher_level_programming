87888def294241b2dbfe3035597bd92591752a5f
isagarazadeh <garazadeisa625@gmail.com>
2024-02-04 10:17:43 -0800

    2nd task

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square with private size attribute"""
+
+
+class Square():
+    """Documentation for Class:
+    This class contains size attribute."""
+    def __init__(self, size):
+        """Initial function for Square class"""
+        self.__size = size
