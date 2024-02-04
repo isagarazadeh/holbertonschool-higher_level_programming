d0e301dabe771b5c3afcafee1b5a53c3375b797e
isagarazadeh <garazadeisa625@gmail.com>
2024-02-04 07:51:06 -0800

    4th

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    od_set = []
+    for i in set_1:
+        if i in set_2:
+            continue
+        else:
+            od_set.append(i)
+    return od_seti
