e237ece405da68d5a01d6a47d54e84729d4e7d9b
isagarazadeh <garazadeisa625@gmail.com>
2024-02-11 11:45:44 -0800

    8th task

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+8-class_to_json.py
+
+
+'''
+
+def class_to_json(obj):
+    '''
+    class_to_json returns the dictionary description with
+    simple data structure for JSON serialization of an object
+
+    Args:
+        obj: object
+
+    '''
+    return obj.__dict__i
