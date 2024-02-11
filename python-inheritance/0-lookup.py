cdf5dbbd8bb5317ddbdbc41b3d69dc9968feb1fb
isagarazadeh <garazadeisa625@gmail.com>
2024-02-11 10:15:46 -0800

    0th

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module helps you to lookup into classes
+'''
+
+
+def lookup(obj):
+    '''
+    lookup helps you to look into the objects
+
+    Args:
+        obj: object
+    '''
+    return dir(obj)
