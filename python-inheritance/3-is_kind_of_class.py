43b72610573221b057f772303be0ae00c0c82fe5
isagarazadeh <garazadeisa625@gmail.com>
2024-02-11 10:32:41 -0800

    3rd task is gone

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module_3
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+    is_kind_of_class checks if obj is an instance of a_class
+
+    Args:
+        obj: object
+        a_class: class
+    '''
+    return isinstance(obj, a_class)
