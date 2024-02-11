01d8798d39d6fde6182aa77e5127d6a96530acc9
isagarazadeh <garazadeisa625@gmail.com>
2024-02-11 11:35:50 -0800

    4th task

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+4-from_json_string.py
+'''
+import json
+
+
+def from_json_string(my_str):
+    '''
+    from_json_string returns an object represented by a JSON string
+    Args:
+        my_str: string
+    '''
+    return json.loads(my_str)
