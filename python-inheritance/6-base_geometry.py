c242bd6772272e118e0c2dc9b522958cd361e4c9
isagarazadeh <garazadeisa625@gmail.com>
2024-02-11 10:48:32 -0800

    6th task is been done

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Module-6
+'''
+
+
+class BaseGeometry:
+    '''
+    My BaseGeometry class
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
