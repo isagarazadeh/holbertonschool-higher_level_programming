4498c6bce2f8c4cc30d828b714cd9d2db35cfb1d
root <garazadeisa625@gmail.com>
2024-03-23 20:26:24 +0000

    9-full_creation.sql

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,3 +4,9 @@ CREATE IF NOT EXISTS TABLE second_table(
 	name VARCHAR(256),
 	score INT
 );
+INSERT INTO second_table(id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
